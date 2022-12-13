0a2b037ca773bbdc352ea6504af12915bd6e4447
Tr-Junior <8xnecrox8@gmail.com>
2022-12-13 12:00:29 -0300

    C# Dapper Sql Server

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using Dapper.Contrib.Extensions;
+using Microsoft.Data.SqlClient;
+
+namespace Blog
+{
+    class Program
+    {
+        private const string CONNECTION_STRING = @"Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$!;Trusted_Connection=False; TrustServerCertificate=True;";
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("");
+        }
+        public static void ReadUsers()
+        {
+            using (var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                var users = connection.GetAll<User>();
+
+                foreach (var user in users)
+                {
+                    System.Console.WriteLine(user.Name);
+                }
+            }
+        }
+    }
+}
